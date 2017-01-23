b451f6f3f4d93c9d676004c348f3179e3c650aaa
tiroshanw <tiroshanm@gmail.com>
2017-01-23 14:30:57 +0530

    add comments

--- instance-based/k-nearest-neighbour/python/k-NN.py
+++ instance-based/k-nearest-neighbour/python/k-NN.py
@@ -1,14 +1,23 @@
-from sklearn.decomposition import PCA
+# -----------------------------------------------------------#
+#    k-Nearest Neighbour Classification Algorithm Example
+#    Author - Tiroshan Madushanka
+# -----------------------------------------------------------#
 import numpy as np
 import pandas as pd
 import math
 import operator
 import graph
+from sklearn.decomposition import PCA
 
+# Load data from an external file and store it at Dataframe (pandas).
 def load_data_set(file_name):
     data = np.loadtxt(file_name,dtype=str,delimiter=",")
     return pd.DataFrame(np.array(data))
 
+# Randomly separate Test Data Set and Training Data Set
+# Default split,
+#  training set = 0.8
+#  test set     = 0.2
 def split_train_test(data_set, split=0.8):
     msk = np.random.rand(len(data_set)) < split
     training = data_set[msk]
@@ -17,6 +26,10 @@ def split_train_test(data_set, split=0.8):
     num_test_set = test[[0, 1, 2, 3, 4, 5, 6, 7, 8]].apply(pd.to_numeric)
     return num_training_set, num_test_set
 
+# Find a k nearest neighbours for a given test_element from training set
+# Algorithm calculate distance between two points using euclidean distance
+#   -User can implement other distance calculations (cosine, Hamming .etc)
+# order candidate data points w.r.t distance and select k no of neighours
 def find_neighbors(training_set, test_element, k):
 	distances = []
 	length = len(test_element)-1
@@ -29,12 +42,15 @@ def find_neighbors(training_set, test_element, k):
 		neighbors.append(distances[x][0])
 	return neighbors
 
+# Calculate Euclidean Distance between two vectors
+#   d(p,q) = sqrt([p1-q1]^2 + [p1-q1]^2 + ... + [pn-qn]^2 )
 def get_euclidean_distance(x1, x2, length):
 	distance = 0
 	for x in range(length):
 		distance += pow((x1[x] - x2[x]), 2)
 	return math.sqrt(distance)
 
+#  Select Class label from Majority Voting
 def get_response(neighbors):
 	class_vote = {}
 	for x in range(len(neighbors)):
@@ -46,6 +62,8 @@ def get_response(neighbors):
 	sorted_votes = sorted(class_vote.iteritems(), key=operator.itemgetter(1), reverse=True)
 	return sorted_votes[0][0]
 
+# Calculate Accuracy - check predicted class label and Test data set labels
+#   and count correct labels out ot test data set
 def calculate_accuracy(test_set, predictions):
 	correct = 0
 	for x in range(len(test_set)):
@@ -57,9 +75,9 @@ def main():
     # Load Sample Data & Training - Test data preparation
     data_set = load_data_set('iris.data')
 
-    split = 0.5
+    # Split data as training and test
+    split = 0.8
     training_set, test_set = split_train_test(data_set, split)
-
 
     print 'Train set: ' + repr(len(training_set))
     print 'Test set: ' + repr(len(test_set))
@@ -71,13 +89,17 @@ def main():
     Y = data_set[8].apply(pd.to_numeric)
 
     # Data visualization
+    # Visualise data distribution w.r.t Sepal length and width
     graph.draw_2d_plot(1, 8, 6, 'Sepal length', 'Sepal width', np.array(Y), np.array(X), np.array(Y))
 
     num_data_set = data_set[[0, 1, 2, 3, 4, 5, 6, 7]].apply(pd.to_numeric)
 
+    # Use Principle Component Analysis and reduce dimension of the data set into 3
     X_reduced = PCA(n_components=3).fit_transform(np.array(num_data_set))
+    # Graph Real data nodes with lables
     graph.draw_3d_plot(2, "First three PCA directions of Data", 8, 6, "1st eigenvector", "2nd eigenvector", "3rd eigenvector", np.array(Y), X_reduced, np.array(Y))
 
+    # Predice Class labels for the test set
     predictions = []
     test_set_array = np.array(test_set)
     train_set_array = np.array(training_set)
@@ -99,8 +121,10 @@ def main():
     results_Y.extend(predictions)
     results_Y.extend(train_set_array[:,8])
 
-    X_reduced = PCA(n_components=3).fit_transform(np.array(results_X))
-    graph.draw_3d_plot(3, "First three PCA directions - with Predicted Results", 8, 6, "1st eigenvector", "2nd eigenvector", "3rd eigenvector", np.array(results_Y), X_reduced, np.array(results_Y))
+    X_reduced_predicted = PCA(n_components=3).fit_transform(np.array(results_X))
+
+    # Graph data with predicted labels
+    graph.draw_3d_plot(3, "First three PCA directions - with Predicted Results", 8, 6, "1st eigenvector", "2nd eigenvector", "3rd eigenvector", np.array(results_Y), X_reduced_predicted, np.array(results_Y))
 
     graph.draw_pyplot()
     # End of Data Visualization
